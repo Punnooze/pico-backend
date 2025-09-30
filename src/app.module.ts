15af810ea1e6bf72f9bffafc9ac8c911eb1bac37
Punnoose Punnen <punnoose.punnen@goniyo.com>
2025-10-01 00:48:55 +0530

    mongodb setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import key from './config/key';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(key.mongoUri), ],
   controllers: [AppController],
   providers: [AppService],
 })
