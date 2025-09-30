31495d65d05278c58b3d3c0bdcc288d825cca01d
Punnoose Punnen <punnoose.punnen@goniyo.com>
2025-10-01 01:59:37 +0530

    tasks crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TaskDto } from './dto/task.dto';
+import { Task } from './interfaces/task.interface';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  createTask(@Body() taskDto: TaskDto) {
+    return this.tasksService.createTask(taskDto);
+  }
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id' ) id) : Promise<Task | null> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Put(':id')
+  updateTask(@Body() updatedTask: TaskDto, @Param('id') id): Promise<Task | null> {
+    return this.tasksService.updateTask(id, updatedTask);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id) : Promise<Task | null> {
+    return this.tasksService.deleteTask(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './interfaces/task.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { TaskDto } from './dto/task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) {}
+
+  async createTask(taskDto: TaskDto): Promise<Task> {
+    const newTask = new this.taskModel(taskDto);
+    return newTask.save();
+  }
+
+  async getAllTasks(): Promise<Task[]> {
+    return await this.taskModel.find();
+  }
+
+  async getTaskById(id: string): Promise<Task | null> {
+    return await this.taskModel.findOne({_id: id});
+  }
+
+  async updateTask(id: String, taskDto: TaskDto): Promise<Task | null> {
+    const updatedTask = await this.taskModel.findByIdAndUpdate(id, taskDto, {new: true});
+    return updatedTask;
+  }
+
+  async deleteTask(id: String): Promise<Task | null>{
+    const deleteTask = await this.taskModel.findOneAndDelete(id);
+    return deleteTask;
+  }
+}
